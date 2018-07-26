ee43522e250ad9387b85fab468300127f4acc841
cousinojake <cousinoj@mail.gvsu.edu>
2018-07-26 18:32:01 -0400

    Have logic of adding hash values to the list and widdling down to top hash but incorrect value.

--- Hash.c
+++ Hash.c
@@ -22,9 +22,10 @@ int main()
   // SHA1_CTX sha;
   // uint8_t results[20];
   // // char *result1;
-  // char *buf;
+  // // char *buf;
   // int n;
-  // buf = "abc";
+  // // buf = "abc";
+  // char buf[3] = "abc";
   // n = strlen(buf);
   // SHA1Init(&sha);
   // SHA1Update(&sha, (uint8_t *)buf, n);
@@ -40,9 +41,9 @@ int main()
   // printf("\n%s\n", "0xa9993e364706816aba3e25717850c26c9cd0d89d");
 
 
-/**
+/*
   start a second linked list to hold all of the hashes. then when finished, start from tail and work to the head.
-/**/
+*/
 
   struct dirent *dirent;
 
@@ -55,34 +56,91 @@ int main()
   {
     if(strncmp(dirent->d_name, ".", 1) != 0)
     {
-      printf("%s\n", dirent->d_name);
+      // printf("%s\n", dirent->d_name);
       insertItem(&MainList, dirent->d_name);
       fileCount++;
     }
   }
 
+  ListNodePtr holdingList = NULL;
+  holdingList = MainList;
+
+  SHA1_CTX sha;
+  SHA1Init(&sha);
+
   while(fileCount > 1)
   {
     fileCount = ((fileCount / 2) + (fileCount % 2));
+
+    ListNodePtr current = NULL;
+    ListNodePtr next = NULL;
     ListNodePtr newList = NULL;
+
     for(int i = 0; i < fileCount; i++)
     {
+      current = holdingList;
+      next = holdingList->nextPtr;
+      printf("%s%s\n", "Current: ", current->data);
+      printf("%s%s\n", "Next: ", next->data);
+      char addedHash[40];
 
+      uint8_t results[20];
+      int n = strlen(current->data);
+      SHA1Update(&sha, (uint8_t *)current->data, n);
+      SHA1Final(results, &sha);
+
+      for(int i = 0; i< 20; i++)
+      {
+        printf("%01x", results[i]);
+        addedHash[i] = (char)results[i];
+      }
+      printf("\n");
+
+      for(int i = 0; i< 20; i++)
+      {
+        printf("%01x", addedHash[i]);
+      }
+      printf("\n");
+
+      if(next != NULL)
+      {
+        n = strlen(next->data);
+        SHA1Update(&sha, (uint8_t *)next->data, n);
+        SHA1Final(results, &sha);
+
+        for(int i = 20; i< 40; i++)
+        {
+          addedHash[i] = (char)results[i-20];
+        }
+        n = strlen(addedHash);
+        SHA1Update(&sha, (uint8_t *)addedHash, n);
+        SHA1Final(results, &sha);
+      }
+
+      char hashResult[20];
+      for(int i = 0; i< 20; i++)
+      {
+        hashResult[i] = (char)results[i];
+        printf("%01x", hashResult[i]);
+      }
+      printf("\n");
+
+      insertItem(&newList, hashResult);
+
+      if(next != NULL)
+      {
+        if(next->nextPtr != NULL) holdingList = next->nextPtr;
+      }
     }
 
     if(fileCount == 1)
     {
       printf("Top Hash: Hello!\n");
+      printf("%s%s\n", "Top Hash: ", newList->data);
     }
+    holdingList = newList;
 
   }
-  // int myarray[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-  // while(1)
-  // {
-  //   int size = (sizeof(myarray) / sizeof(myarray[0]));
-  //   printf("%s%d\n", "Array size is: ", size);
-  //   break;
-  // }
 
   closedir(dr);
   return 0;
@@ -102,6 +160,7 @@ void insertItem( ListNodePtr *sPtr, char value[20] )
           newPtr->data[i] = value[i];
         }
         newPtr->nextPtr = NULL;
+        printf("%s%s\n", "Insert: ", newPtr->data);
 
         previousPtr = NULL;
         currentPtr = *sPtr; // set currentPtr to start of list
@@ -124,11 +183,3 @@ void insertItem( ListNodePtr *sPtr, char value[20] )
         printf( "%s not inserted. No memory available.\n", value );
     } // end else
 } // end function insertItem
-
-
-
-
-// int GetArraySize(int array[])
-// {
-//   return (int)(sizeof(array) / sizeof(int));
-// }
