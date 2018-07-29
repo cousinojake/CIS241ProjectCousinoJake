073df8db4b908a787bf57c6e57cb096c2052a829
cousinojake <cousinoj@mail.gvsu.edu>
2018-07-29 08:50:43 -0400

    Cleaned up Hash.c and added testing files.

--- Hash.c
+++ Hash.c
@@ -4,7 +4,6 @@
 #include <stdlib.h>
 #include "sha1-master/sha1.c"
 
-// int GetArraySize(int array[]);
 // ListNode structure definition
 struct ListNode {
     char data[20]; // node data
@@ -18,45 +17,20 @@ void insertItem( ListNodePtr *sPtr, char value[20] );
 
 int main()
 {
-
-  // SHA1_CTX sha;
-  // uint8_t results[20];
-  // // char *result1;
-  // // char *buf;
-  // int n;
-  // // buf = "abc";
-  // char buf[3] = "abc";
-  // n = strlen(buf);
-  // SHA1Init(&sha);
-  // SHA1Update(&sha, (uint8_t *)buf, n);
-  // SHA1Final(results, &sha);
-  // // SHA1Final(result1, &sha);
-  //
-  // /* Print the digest as one long hex value */
-  // printf("0x");
-  // for (n = 0; n < 20; n++)
-  // {
-  //   printf("%02x", results[n]);
-  // }
-  // printf("\n%s\n", "0xa9993e364706816aba3e25717850c26c9cd0d89d");
-
-
-/*
-  start a second linked list to hold all of the hashes. then when finished, start from tail and work to the head.
-*/
-
   struct dirent *dirent;
 
-  DIR *dr = opendir(".");
+  char * filePath = "sample_files/";
+  DIR *dr = opendir(filePath);
   int fileCount = 0;
 
   ListNodePtr MainList = NULL;
 
+  printf("%s%s\n", "Files in ", filePath);
   while((dirent = readdir(dr)) != NULL)
   {
     if(strncmp(dirent->d_name, ".", 1) != 0)
     {
-      // printf("%s\n", dirent->d_name);
+      printf("%s\n", dirent->d_name);
       insertItem(&MainList, dirent->d_name);
       fileCount++;
     }
@@ -80,8 +54,8 @@ int main()
     {
       current = holdingList;
       next = holdingList->nextPtr;
-      printf("%s%s\n", "Current: ", current->data);
-      printf("%s%s\n", "Next: ", next->data);
+      // printf("%s%s\n", "Current: ", current->data);
+      // printf("%s%s\n", "Next: ", next->data);
       char addedHash[100];
 
       uint8_t results[20];
@@ -89,24 +63,10 @@ int main()
       SHA1Update(&sha, (uint8_t *)current->data, n);
       SHA1Final(results, &sha);
 
-      for(int i = 0; i< 20; i++)
-      {
-        printf("%02x", results[i]); //hex code!
-
-      //  addedHash[i] = (char)results[i];
-      }
       char firstResult[50], secondResult[50];
-      sprintf(firstResult, "%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x\0",
+      sprintf(firstResult, "%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
         results[0], results[1], results[2], results[3], results[4], results[5], results[6], results[7], results[8], results[9],
         results[10], results[11], results[12], results[13], results[14], results[15], results[16], results[17], results[18], results[19]);
-
-      printf("\nFirstResult: %s\n", firstResult);
-      // printf("\nresults numeric values:\n");
-      // for(int i = 0; i< 20; i++)
-      // {
-      //   printf("%u\n", results[i]);
-      // }
-      printf("\n");
 
       if(next != NULL)
       {
@@ -114,30 +74,15 @@ int main()
         SHA1Update(&sha, (uint8_t *)next->data, n);
         SHA1Final(results, &sha);
 
-        sprintf(secondResult, "%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x\0",
+        sprintf(secondResult, "%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
           results[0], results[1], results[2], results[3], results[4], results[5], results[6], results[7], results[8], results[9],
           results[10], results[11], results[12], results[13], results[14], results[15], results[16], results[17], results[18], results[19]);
-
-        // for(int i = 20; i< 40; i++)
-        // {
-        //   addedHash[i] = (char)results[i-20];
-        // }
       }
-        sprintf(addedHash, "%s%s", firstResult, secondResult);
-        n = strlen(addedHash);
-        SHA1Update(&sha, (uint8_t *)addedHash, n);
-        SHA1Final(results, &sha);
+      sprintf(addedHash, "%s%s", firstResult, secondResult);
+      n = strlen(addedHash);
+      SHA1Update(&sha, (uint8_t *)addedHash, n);
+      SHA1Final(results, &sha);
 
-
-      // char hashResult[20];
-      // for(int i = 0; i< 20; i++)
-      // {
-      //   hashResult[i] = (char)results[i];
-      //   // printf("%02x", hashResult[i]);
-      // }
-      // printf("\n");
-
-      // insertItem(&newList, hashResult);
       insertItem(&newList, addedHash);
 
       if(next != NULL)
@@ -148,7 +93,6 @@ int main()
 
     if(fileCount == 1)
     {
-      printf("Top Hash: Hello!\n");
       printf("%s%s\n", "Top Hash: ", newList->data);
     }
     holdingList = newList;
@@ -173,7 +117,6 @@ void insertItem( ListNodePtr *sPtr, char value[20] )
           newPtr->data[i] = value[i];
         }
         newPtr->nextPtr = NULL;
-        printf("%s%s\n", "Insert: ", newPtr->data);
 
         previousPtr = NULL;
         currentPtr = *sPtr; // set currentPtr to start of list
